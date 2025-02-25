a3dbd68affd50af531a98dc162073035d3d23051
Yancy marine <yanci.marine@gmail.com>
2025-02-25 18:02:47 -0400

    segundo

---
+++ src/breakingbad/breaking-bad-app.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const BrakingbadApp = (element) => {
+    document.querySelector('#app-title').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Cargando...';
+   console.log('hola mundo');
+
+}
--- src/main.js
+++ src/main.js
@@ -2,23 +2,22 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { BrakingbadApp } from './breakingbad/breaking-bad-app.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="https://vitejs.dev" target="_blank">
+       <img src="${viteLogo}" class="logo" alt="Vite logo"/>
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    <a href="#" target="_blank">
+        <img src="${javascriptLogo}" class="logo vanilla" alt="Javascript logo">
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 id="app-title">http app</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+     
 
-setupCounter(document.querySelector('#counter'))
+    </div>
+ </div>`;
+const elemento = document.querySelector('.card');
+BrakingbadApp(elemento);
+//setupCounter(document.querySelector('#counter'))
